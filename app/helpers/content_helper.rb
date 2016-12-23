093720526919983abe78b3eca1604a79af4e93f0
Robert Cotey <coteyr@coteyr.net>
2016-12-23 08:19:09 -0500

    Issue #25305
    Lots of code clean up

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -1,7 +1,7 @@
 # @Author: Robert D. Cotey II <coteyr@coteyr.net>
 # @Date:   2016-12-22 13:22:09
 # @Last Modified by:   Robert D. Cotey II <coteyr@coteyr.net>
-# @Last Modified time: 2016-12-22 14:13:09
+# @Last Modified time: 2016-12-22 14:58:24
 module ContentHelper
   def row(&block)
     to_return = '<div class="row container-fluid">'
@@ -38,6 +38,25 @@ module ContentHelper
   def menu_toggle(title, id, &block)
     to_return = menu_button title, "##{id}", data: {toggle: 'collapse', parent: '#accordion'}, role: 'button'
     to_return += menu_panel id, &block
+    to_return.html_safe
+  end
+  def well(title, options={}, &block)
+    size = 12 || options[:size]
+    to_return =  "<div class='panel panel-default col-md-#{size}'>"
+    to_return += '<div class="panel-heading">'
+
+    to_return += '<h2 class="panel-title">'
+    if options[:subheading]
+      to_return += '<h6 class="pull-right" style="position: relative;top: 50%;transform: translateY(-50%);">'
+      to_return += options[:subheading]
+      to_return += '</h6>'
+    end
+    to_return += title
+    to_return += '</h2>'
+
+    to_return += '</div>'
+    to_return += with_output_buffer(&block)
+    to_return += '</div>'
     to_return.html_safe
   end
 end

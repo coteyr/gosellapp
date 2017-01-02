79bb94ce60be0f009ea3f2d8580cca32201127a7
Robert Cotey <coteyr@coteyr.net>
2017-01-02 11:02:35 -0500

    Issue #25268
    nested forms

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -5,6 +5,10 @@ class ResultsController < ApplicationController
     @result = @prospect.results.new
     @result.prospect = @prospect
     if @result.update_attributes result_params
+      @note = @prospect.notes.new
+      @note.detail = params[:note]
+      @note.detail = @result.disposition if @note.detail.blank?
+      @note.save(validate: false)
       @prospect.update_column :canvassed, true if params[:go] == 'walk'
       @prospect.update_column :called, true if params[:go] == 'smile'
       redirect_to prospects_path(go: params[:go]), notice: 'Result was successfully posted.'
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,4 @@
 class Note < ApplicationRecord
   belongs_to :prospect, dependent: :destroy
+  validates :detail, presence: true
 end

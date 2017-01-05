fc59069e2946a902aec3887268e48cb0ef76d32a
Robert Cotey <coteyr@coteyr.net>
2017-01-05 13:05:31 -0500

    Issue #25289
    group seperation

--- db/migrate/20170105165325_add_group_id_to_lists.rb
+++ db/migrate/20170105165325_add_group_id_to_lists.rb
@@ -1,6 +1,6 @@
 class AddGroupIdToLists < ActiveRecord::Migration[5.0]
   def change
     add_column :lists, :group_id, :integer
-    remove_column :lists, :user_id
+    # remove_column :lists, :user_id
   end
 end
